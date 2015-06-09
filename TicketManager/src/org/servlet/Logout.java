cac550007a7441f398a356d29f6c8dcee72ab922
dheerajkumar147@gmail.com <dheerajkumar147@gmail.com@f54d431b-3b16-9c87-bda3-b489ca4e0068>
2015-06-09 15:22:23 +0000

    Initial import.

---
+++ TicketManager/src/org/servlet/Logout.java
@@ -0,0 +1,39 @@
+package org.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.main.ConnectionManager;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getSession().invalidate();
+	    String status = new ConnectionManager().ConnClose();
+	    System.out.println("Status of Connection --->" + status);
+	    System.out.println("Calling logout class");
+	    response.sendRedirect(request.getContextPath() + "/logout.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
